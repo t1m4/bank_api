6f40bc497d96faff7dfa4e4bc37024056b2ccbe6
Ruslan Hamidullin <ruslan.hamidullin@starnavi.io>
2021-09-07 14:57:11 +0300

    Add BankAccount model users app

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from users.models import BankAccount
+
+admin.site.register(BankAccount)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class BankAccount(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
