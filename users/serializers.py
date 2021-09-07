a2d90c717a1b0accb0a020894491525a7d5f1271
Ruslan Hamidullin <ruslan.hamidullin@starnavi.io>
2021-09-07 17:28:55 +0300

    Add transfer money endpoint for users app

--- users/serializers.py
+++ users/serializers.py
@@ -15,3 +15,18 @@ class OperationSerializer(serializers.ModelSerializer):
         model = Operation
         fields = ['amount', 'message', 'sender', 'receiver']
 
+    def validate(self, data):
+        if data['amount'] > data['sender'].balance:
+            raise serializers.ValidationError("Sender don't have enough money.")
+        if data['sender'] == data['receiver']:
+            raise serializers.ValidationError("Sender and receiver must not be equal.")
+        return data
+
+    def save(self, **kwargs):
+        amount = self.validated_data['amount']
+        with transaction.atomic():
+            self.validated_data['sender'].balance -= amount
+            self.validated_data['receiver'].balance += amount
+            self.validated_data['sender'].save(update_fields=['balance'])
+            self.validated_data['receiver'].save(update_fields=['balance'])
+            return super(OperationSerializer, self).save(**kwargs)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import generics
+
+from users.serializers import BankAccountSerializer, OperationSerializer
+
+
+class CreateBankAccount(generics.CreateAPIView):
+    serializer_class = BankAccountSerializer
+
+
+class TransferView(generics.CreateAPIView):
+    serializer_class = OperationSerializer
