3a07f177c74db5cb940119b348d00ede2b535c9d
Ruslan Hamidullin <ruslan.hamidullin@starnavi.io>
2021-09-07 14:47:26 +0300

    Add new app users

--- test_project/settings.py
+++ test_project/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+INSTALLED_APPS += [
+    'users',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
